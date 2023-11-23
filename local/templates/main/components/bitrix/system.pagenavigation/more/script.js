f7d4192dd7e710a56ba5d5e7ef25fdb569d4bdf2
John Doe <johndoe@example.com>
2023-11-23 14:50:10 +0300

    trash

--- local/templates/main/components/bitrix/system.pagenavigation/more/script.js
+++ local/templates/main/components/bitrix/system.pagenavigation/more/script.js
@@ -1,20 +1,19 @@
-$(document).ready(function () {
+$(document).ready(function(){
 
-    $(document).on('click', '.load-more-items', function () {
+    $(document).on('click', '.load-more-items', function(){
         var targetContainer = $('.items-list'),
-            url = $('.load-more-items').attr('data-url');
+            url =  $('.load-more-items').attr('data-url');
         if (url !== undefined) {
             $.ajax({
                 type: 'GET',
                 url: url,
                 dataType: 'html',
-                success: function (data) {
+                success: function(data){
                     //$('.load-more-items').remove();
                     var elements = $(data).find('.items-list .item'),
                         paginationUrl = $(data).find('.load-more-items').data('url');
-                    console.log(elements);
                     targetContainer.append(elements);
-                    if (paginationUrl)
+                    if(paginationUrl)
                         $('.load-more-items').attr('data-url', paginationUrl);
                     else
                         $('.load-more-items').hide();
