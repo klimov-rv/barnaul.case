8dbd0a93c7d4404425fb0c3f5c7e88f0f264fa6d
John Doe <johndoe@example.com>
2023-11-23 14:43:52 +0300

    main_event done

---
+++ about-barnaul/main_event/include_images.php
@@ -0,0 +1,6 @@
+<div class="main-event__img cell-12 cell-6-m cell-12-xs">
+    <img src="/about-barnaul/main_event/6fasad.jpg" alt="">
+</div>
+<div class="main-event__img cell-12 cell-6-m cell-12-xs">
+    <img src="/about-barnaul/main_event/IMG-20221028-WA0008.jpg" alt="">
+</div>
