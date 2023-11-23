fb9c6a92737e1bb4c0b824c43f88a6835df78f26
John Doe <johndoe@example.com>
2023-11-23 14:44:04 +0300

    add console_log

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,2 +1,9 @@
 <?php
 \Bitrix\Main\Loader::includeModule("site.main");
+
+function console_log($data)
+{
+    echo '<script>';
+    echo 'console.log(' . json_encode($data) . ')';
+    echo '</script>';
+}
