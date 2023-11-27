dc6ef62e361a1a5f5a21fd23700202e2624ee6a5
John Doe <johndoe@example.com>
2023-11-27 12:58:55 +0300

    main_event title fix

--- about-barnaul/main_event/index.php
+++ about-barnaul/main_event/index.php
@@ -4,8 +4,8 @@ $APPLICATION->SetTitle("Главное событие в городе");
 ?>
 <article class="main_event">
 	<div class="container">
-	<span class="welcome_text">
-		Барнаул – один из крупнейших сибирских городов и столица Алтайского края. Город расположен в равнинной местности, одновременно на двух реках: реке Барнаулке и левом берегу полноводной сибирской реки Обь, берущей начала в горах Алтая. Большая часть города располагается в Ленточном бору, где берет начало реликтовый ленточный сосновый бор – уникальная растительная зона, не встречающаяся больше нигде на планете. Количество солнечных дней в Барнауле – 230 дней, почти столько же, как в Крыму. </span>
+		<span class="welcome_text">
+			Барнаул – один из крупнейших сибирских городов и столица Алтайского края. Город расположен в равнинной местности, одновременно на двух реках: реке Барнаулке и левом берегу полноводной сибирской реки Обь, берущей начала в горах Алтая. Большая часть города располагается в Ленточном бору, где берет начало реликтовый ленточный сосновый бор – уникальная растительная зона, не встречающаяся больше нигде на планете. Количество солнечных дней в Барнауле – 230 дней, почти столько же, как в Крыму. </span>
 		<div class="welcome_grid row is-grid">
 			<div class="cell-12">
 				<h5 class="section__title">Экскурсии от Анны Казанцевой </h5>
@@ -16,48 +16,48 @@ $APPLICATION->SetTitle("Главное событие в городе");
 				</p>
 				<p>
 					Напоминаем, 23 сентября в честь Всемирного дня туризма пройдёт квест-экскурсия «Живые символы Барнаула».У участников квеста появится возможность найти «следы животных» в истории Барнаула. Квест-экскурсия дважды (12:00 и 15:00) стартует от Демидовской площади. В ходе игры группам предстоит найти живой символ города Барнаула, участники получат ценные призы на память.Чтобы попасть на квест, нужно записаться по телефону63-35-75, либо подойти к палатке Туристского информационного центра на <a href="/">
-					улице Мало-Тобольской в эту субботу. </a>
+						улице Мало-Тобольской в эту субботу. </a>
 				</p>
 				<p>
-	<b>Напоминаем, 23 сентября в честь Всемирного дня туризма пройдёт квест-экскурсия:</b>
+					<b>Напоминаем, 23 сентября в честь Всемирного дня туризма пройдёт квест-экскурсия:</b>
 				</p>
 				<ul>
 					<li> «Живые символы Барнаула».У участников квеста появится возможность найти «следы животных» в истории Барнаула. </li>
 					<li>
-					Квест-экскурсия дважды (12:00 и 15:00) стартует от Демидовской площади. В ходе игры группам предстоит найти живой символ города Барнаула, участники получат ценные призы на память.</li>
+						Квест-экскурсия дважды (12:00 и 15:00) стартует от Демидовской площади. В ходе игры группам предстоит найти живой символ города Барнаула, участники получат ценные призы на память.</li>
 					<li>
-					Чтобы попасть на квест, нужно записаться по телефону63-35-75, либо подойти к палатке Туристского информационного центра на улице Мало-Тобольской в эту субботу. </li>
+						Чтобы попасть на квест, нужно записаться по телефону63-35-75, либо подойти к палатке Туристского информационного центра на улице Мало-Тобольской в эту субботу. </li>
 				</ul>
 				<p>
 				</p>
 				<div class="article_link">
-	<span class="article_link-inner">
-					Подробная информация на странице фестиваля Вконтакте: <a href="https://vk.com/altaitourismday" target="_blank" rel="noopener noreferrer">https://vk.com/altaitourismday</a> </span>
+					<span class="article_link-inner">
+						Подробная информация на странице фестиваля Вконтакте: <a href="https://vk.com/altaitourismday" target="_blank" rel="noopener noreferrer">https://vk.com/altaitourismday</a> </span>
 				</div>
 			</div>
 			<div class="main-event__sidebar cell-3 cell-5-md cell-12-m">
 				<div class="col">
 
-				<?$APPLICATION->IncludeComponent(
-					"bitrix:main.include",
-					"",
-					Array(
-						"AREA_FILE_SHOW" => "file",
-						"AREA_FILE_SUFFIX" => "inc",
-						"EDIT_TEMPLATE" => "",
-						"PATH" => "/about-barnaul/main_event/include_images.php"
-					)
-				);?>
+					<? $APPLICATION->IncludeComponent(
+						"bitrix:main.include",
+						"",
+						array(
+							"AREA_FILE_SHOW" => "file",
+							"AREA_FILE_SUFFIX" => "inc",
+							"EDIT_TEMPLATE" => "",
+							"PATH" => "/about-barnaul/main_event/include_images.php"
+						)
+					); ?>
 				</div>
 			</div>
 		</div>
 	</div>
- </article>
+</article>
 
-<?$APPLICATION->IncludeComponent(
+<? $APPLICATION->IncludeComponent(
 	"bitrix:news",
 	"gallery",
-	Array(
+	array(
 		"IBLOCK_ID" => "10",
 		"COMPONENT_TEMPLATE" => "gallery",
 		"IBLOCK_TYPE" => "data",
@@ -78,11 +78,11 @@ $APPLICATION->SetTitle("Главное событие в городе");
 		"DETAIL_ACTIVE_DATE_FORMAT" => "d.m.Y",
 		"DETAIL_DISPLAY_BOTTOM_PAGER" => "Y",
 		"DETAIL_DISPLAY_TOP_PAGER" => "N",
-		"DETAIL_FIELD_CODE" => array(0=>"",1=>"",),
+		"DETAIL_FIELD_CODE" => array(0 => "", 1 => "",),
 		"DETAIL_PAGER_SHOW_ALL" => "Y",
 		"DETAIL_PAGER_TEMPLATE" => "",
 		"DETAIL_PAGER_TITLE" => "Страница",
-		"DETAIL_PROPERTY_CODE" => array(0=>"",1=>"",),
+		"DETAIL_PROPERTY_CODE" => array(0 => "", 1 => "",),
 		"DETAIL_SET_CANONICAL_URL" => "N",
 		"DISPLAY_BOTTOM_PAGER" => "Y",
 		"DISPLAY_DATE" => "Y",
@@ -92,8 +92,8 @@ $APPLICATION->SetTitle("Главное событие в городе");
 		"DISPLAY_TOP_PAGER" => "N",
 		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
 		"LIST_ACTIVE_DATE_FORMAT" => "d.m.Y",
-		"LIST_FIELD_CODE" => array(0=>"",1=>"",),
-		"LIST_PROPERTY_CODE" => array(0=>"",1=>"",),
+		"LIST_FIELD_CODE" => array(0 => "", 1 => "",),
+		"LIST_PROPERTY_CODE" => array(0 => "", 1 => "",),
 		"MESSAGE_404" => "",
 		"META_DESCRIPTION" => "-",
 		"META_KEYWORDS" => "-",
@@ -109,7 +109,7 @@ $APPLICATION->SetTitle("Главное событие в городе");
 		"SEF_MODE" => "N",
 		"SET_LAST_MODIFIED" => "N",
 		"SET_STATUS_404" => "N",
-		"SET_TITLE" => "N",
+		"SET_TITLE" => "Y",
 		"SHOW_404" => "N",
 		"SORT_BY1" => "ACTIVE_FROM",
 		"SORT_BY2" => "SORT",
@@ -124,7 +124,7 @@ $APPLICATION->SetTitle("Главное событие в городе");
 		"USE_RSS" => "N",
 		"USE_SEARCH" => "N",
 		"USE_SHARE" => "N",
-		"VARIABLE_ALIASES" => array("SECTION_ID"=>"SECTION_ID","ELEMENT_ID"=>"ELEMENT_ID",)
+		"VARIABLE_ALIASES" => array("SECTION_ID" => "SECTION_ID", "ELEMENT_ID" => "ELEMENT_ID",)
 	)
-);?>
+); ?>
 <? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
--- local/templates/main/components/bitrix/news/gallery/bitrix/news.list/.default/component_epilog.php
+++
@@ -1,5 +0,0 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
-
-$APPLICATION->SetTitle($arResult['PAGE_TITLE']);
-if($arResult["ADD_SECTION_IN_BREADCRUMBS"])
-    $APPLICATION->AddChainItem($arResult['PAGE_TITLE'], "");
