007f640c79b9c6b81c0d398066fba071bc0ab475
John Doe <johndoe@example.com>
2023-11-29 15:51:08 +0300

    fix title with gallery component

--- local/templates/main/components/bitrix/news/gallery/bitrix/news.list/.default/component_epilog.php
+++ local/templates/main/components/bitrix/news/gallery/bitrix/news.list/.default/component_epilog.php
@@ -1,5 +1,1 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
-
-$APPLICATION->SetTitle($arResult['PAGE_TITLE']);
-if($arResult["ADD_SECTION_IN_BREADCRUMBS"])
-    $APPLICATION->AddChainItem($arResult['PAGE_TITLE'], "");
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
--- local/templates/main/components/bitrix/news/gallery/bitrix/news.list/.default/result_modifier.php
+++ local/templates/main/components/bitrix/news/gallery/bitrix/news.list/.default/result_modifier.php
@@ -6,7 +6,4 @@ foreach($arResult["ITEMS"] as &$arItem){
         $arItem['PREVIEW_PICTURE'] = CFile::ResizeImageGet($arItem['PREVIEW_PICTURE'], array('width'=>453,'height'=>273), BX_RESIZE_IMAGE_EXACT);
     }
 }
-
-// Установить заголовок страницы
-$arResult["PAGE_TITLE"] = $arParams['SECTION_NAME'];
-$this->getComponent()->setResultCacheKeys(["PAGE_TITLE", "ADD_SECTION_IN_BREADCRUMBS"]);
+ 
